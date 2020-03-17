8ee1ebb79763a0d684ba3982ee9085c171ff45b4
b0rski <11145838+b0rski@users.noreply.github.com>
2020-03-17 13:50:07 +0100

    SSO with https://github.com/dsb-norge/vue-keycloak-js.

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -3,10 +3,6 @@ export const STORAGE_KEY = 'cloud-ssp';
 export const state = {
   backendURL: 'http://localhost:8000',
   wzuURL: 'http://localhost:8001',
-  ssoRealmName: 'SBB_Public',
-  ssoRealmURL: 'https://sso.sbb.ch/auth',
-  ssoClientID: 'd0b48c57',
-  ssoIdpHint: 'adfs_sbb_prod',
   user: JSON.parse(window.localStorage.getItem(STORAGE_KEY) || 'null'),
   notification: {}
 };
