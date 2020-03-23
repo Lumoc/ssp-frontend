5a1ba0db1fd31d77cce407a636215ebdb07dfaa9
b0rski <11145838+b0rski@users.noreply.github.com>
2020-03-23 13:50:06 +0100

    New app feature added to Kafka admin console.

--- src/components/kafka/NewApp.vue
+++ src/components/kafka/NewApp.vue
@@ -6,17 +6,17 @@
       </header>
       <section class="modal-card-body">
         <b-field label="App Name">
-          <b-input v-model="appName" :value="appName" required pattern="[a-z0-9-]*" validation-message="Only lower case letters, numbers and hyphens are allowed."></b-input>
+          <b-input v-model="appName" required pattern="[a-z0-9-]*" validation-message="Only lower case letters, numbers and hyphens are allowed."></b-input>
         </b-field>
         <b-field label="First Admin's E-Mail">
           <b-input type="email" v-model="email" required></b-input>
         </b-field>
         <b-field label="Internal Order">
-          <b-input v-model="internalOrder" :value="internalOrder">
+          <b-input v-model="internalOrder">
           </b-input>
         </b-field>
         <b-field label="PSP Number">
-          <b-input v-model="pspNumber" :value="pspNumber"></b-input>
+          <b-input v-model="pspNumber"></b-input>
         </b-field>
         <b-message title="Creating New App" :active.sync="infoIsActive">
             Please wait, app creation takes a while.
